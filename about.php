b654efc7792d721cfe216a0fc46db24b7ed358b0
Alex Sandro Dabukke <129069950+mrsidabukke@users.noreply.github.com>
2023-06-17 10:49:50 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,15 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+<?php require_once "view/header.php"; ?>
+
+
+<div class="card" style="width: 18rem;">
+  <img src="img/alexv3.jpg" class="card-img-top" alt="gambar pembuat">
+  <div class="card-body">
+    <h5 class="card-title">Creator</h5>
+    <p class="card-text"></p>
+    <a href="index.php" class="btn btn-primary">kembali ke home</a>
+  </div>
+</div>
+
+<?php require_once "view/footer.php"; ?>
